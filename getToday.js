e86a84716050bb97e373f4377c017011b8683be1
Lukas Hermann <hey@lukashermann.dev>
2024-01-18 16:55:58 +0100

    docs: minor simplification of getToday

--- getToday.js
+++ getToday.js
@@ -5,11 +5,11 @@ import addMilliseconds from 'date-fns/addMilliseconds'
  * Get the Date of 0:00 today in the given timezone aware
  *
  * @param  {string} [timezone] - assumes 'UTC' if empty
- * @param  {Date|string} [now] - provide a date for 'today', used for testing
+ * @param  {Date} [now] - provide a date for 'today', used for testing
  * @return {Date}
  */
 export default function getToday (timezone = undefined, now = undefined) {
-  const today = now ? new Date(now) : new Date()
+  const today = now || new Date()
   const tzOffset = timezone ? getTimezoneOffset(timezone, today) : 0
   const zoned = addMilliseconds(today, tzOffset)
   zoned.setUTCHours(0, 0, 0, 0)
