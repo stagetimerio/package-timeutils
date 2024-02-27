ca4686d475b2d0a5028207f29bafafb35306bb0e
Lukas Hermann <hey@lukashermann.dev>
2024-02-27 11:18:47 +0100

    chore: remove toYYYYMMDD

--- toYYYYMMDD.DEPRECATED.js
+++
@@ -1,6 +0,0 @@
-import format from 'date-fns/format/index.js'
-
-export default function toYYYYMMDD (date, { asUTC = false } = {}) {
-  if (asUTC) return date.toISOString().split('T')[0]
-  else return format(date, 'yyyy-MM-dd')
-}
